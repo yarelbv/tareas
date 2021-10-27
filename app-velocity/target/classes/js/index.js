7189737f3abb46ad9f9b897ec45b35c939801a5b
yarelbv <89542355+yarelbv@users.noreply.github.com>
2021-10-27 13:04:09 -0500

    Add files via upload

---
+++ app-velocity/target/classes/js/index.js
@@ -0,0 +1,50 @@
+var correo;
+var id;
+
+var btnEnviar = document.getElementById("btnEnviar");
+btnEnviar.addEventListener("click", () => {
+    axios.post("https://app-velocity.herokuapp.com/enviar", {
+            email: document.getElementById("email").value,
+            password: document.getElementById("password").value
+        })
+        .then(function(res) {
+            alert("Usuario:" + res.data.status + " id:" + res.data.id);
+            correo = document.getElementById("email").value;
+            id = res.data.id;
+        })
+        .catch(function(error) {
+            console.log(error)
+        })
+});
+
+var btnLista = document.getElementById("btnLista");
+btnLista.addEventListener("click", () => {
+    let params = new URLSearchParams();
+    params.append("email", correo);
+    params.append("id", id);
+    window.location.replace("http://127.0.0.1:5500/lista.html?" + params);
+});
+
+var btnTabla = document.getElementById("btnTabla");
+btnTabla.addEventListener("click", () => {
+    axios.get("http://localhost:4567/usuarios")
+        .then(function(res) {
+            let json = res.data;
+            let listaTareas = document.getElementById("usuarios");
+            for (var clave in json) {
+
+                if (json.hasOwnProperty(clave)) {
+
+                    let tarea = document.createElement("li");
+                    tarea.textContent = clave + " " + json[clave].email;
+                    listaTareas.appendChild(tarea);
+                }
+            }
+        })
+        .catch()
+})
+
+var btnRedireccion = document.getElementById("btnRedireccion");
+btnRedireccion.addEventListener("click", () => {
+    window.location.replace("http://localhost:4567/hola");
+});
